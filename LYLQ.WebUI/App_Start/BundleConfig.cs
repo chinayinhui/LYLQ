d771b175964ce20ba7cf4007316f1f9542b8c428
chinayinhui <82286408@qq.com>
2015-11-13 22:48:21 +0800

    Updated bootstrap js and css.

--- LYLQ.WebUI/App_Start/BundleConfig.cs
+++ LYLQ.WebUI/App_Start/BundleConfig.cs
@@ -26,9 +26,9 @@ namespace LYLQ.WebUI
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.min.js"));            
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));            
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css", "~/Content/bootstrap_min.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css", "~/Content/bootstrap.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
