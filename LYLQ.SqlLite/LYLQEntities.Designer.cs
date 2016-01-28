cfccb4e7ad4d00a850de25e3cff47b676288bce2
chinayinhui <yhhjf78910>
2016-01-28 19:40:21 +0800

    Updated changes

--- LYLQ.SqlLite/LYLQEntities.Designer.cs
+++ LYLQ.SqlLite/LYLQEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Administrator\Desktop\LYLQ_公司\LYLQ_VS2013\LYLQ.SqlLite\LYLQEntities.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\GitHub\LYLQ\LYLQ.SqlLite\LYLQEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
